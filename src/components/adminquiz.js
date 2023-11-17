fe5d6af22fa17704f64be0bbada0145d60fdfbed
Sasidev <conorken1@gmail.com>
2023-11-17 23:48:01 +0530

    Update adminquiz.js

--- src/components/adminquiz.js
+++ src/components/adminquiz.js
@@ -12,6 +12,15 @@ function Adminquiz(props) {
     const addRow = () => {
         setQcount(qcount + 1)
     }
+      function isImgUrl(url) {
+        const img = new Image();
+        img.src = url;
+        return new Promise((resolve) => {
+          img.onload = () => resolve(true);
+          img.onerror = () => resolve(false);
+        });
+      }
+
 
 
 
